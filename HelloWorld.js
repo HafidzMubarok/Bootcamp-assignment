ac854a834402fdb12e48f38ad23cb216ac630f70
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-07-23 11:21:48 +0700

    Change file name & add text value

--- HelloWorld.js
+++
@@ -1,6 +0,0 @@
-let nama = 'Fathul'
-let umur = '23'
-
-let text = "Perkenalkan nama saya " + nama + " " + "Umur saya saat ini " + umur
-
-console.info(text);
---
+++ Perkenalan.js
@@ -0,0 +1,11 @@
+let nama = 'Fathul'
+let umur = '23'
+let domicile = 'Cibiru, Bandung'
+let hobbyList = ['Musik', 'Berkegiatan di alam bebas']
+let hobby = hobbyList.toString();
+
+let text =
+    "Perkenalkan nama saya " + nama + ". " + "Umur saya saat ini " + umur + " tahun. " + "Saya berdomisili di " + domicile
+    + ". " + "Hobi saya diantaranya " + hobby
+
+console.info(text);
