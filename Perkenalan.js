b97fd9a8a042d5179c911ce645003bfbc3559a22
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-07-23 11:58:08 +0700

    Change text string to backtick

--- Perkenalan.js
+++ Perkenalan.js
@@ -1,11 +1,10 @@
 let nama = 'Fathul'
-let umur = '23'
+let age = '23'
 let domicile = 'Cibiru, Bandung'
 let hobbyList = ['Musik', 'Berkegiatan di alam bebas']
 let hobby = hobbyList.toString();
 
 let text =
-    "Perkenalkan nama saya " + nama + ". " + "Umur saya saat ini " + umur + " tahun. " + "Saya berdomisili di " + domicile
-    + ". " + "Hobi saya diantaranya " + hobby
+    `Perkenalkan nama saya ${nama}. Umur saya saat ini ${age} tahun. Saya berdomisili di ${domicile}. Hobi saya diantaranya ${hobby}`
 
 console.info(text);
