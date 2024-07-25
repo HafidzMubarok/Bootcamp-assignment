b0057bd3f98ea3a9349f0aba84d183fef5d96e00
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-07-25 11:14:38 +0700

    :hammer: Make json file from identity question function

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const fs = require('fs');
 const readline = require('node:readline');
-var validator = require('validator');
+const validator = require('validator');
 
 const { stdin: input, stdout: output } = require('node:process');
-
 const rl = readline.createInterface({ input, output });
-var isIdentityValid = false;
+
+const getDataContacts = (callback) => {
+    const file = './data/contacts.json';
+
+    fs.access(file, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log(`${file} does not exist. Creating...`);
+            fs.writeFileSync(file, '[]');
+            callback([]);
+        } else {
+            try {
+                const data = fs.readFileSync(file, 'utf-8');
+                if (data) {
+                    const jsonData = JSON.parse(data);
+                    callback(jsonData);
+                } else {
+                    console.log('File is empty or content is not valid JSON.');
+                    callback([]);
+                }
+            } catch (error) {
+                console.error('Error parsing JSON:', error.message);
+                callback([]);
+            }
+        }
+    });
+};
+
+const pushDataContacts = (newContact) => {
+    getDataContacts((contacts) => {
+        contacts.push(newContact);
+        fs.writeFileSync('./data/contacts.json', JSON.stringify(contacts, null, 2));
+    });
+};
 
 const identityQuestion = () => {
     rl.question('Your name: ', (name) => {
@@ -13,22 +44,21 @@ const identityQuestion = () => {
             if (validator.isEmail(email)) {
                 rl.question('Phone number: ', (phone) => {
                     if (validator.isMobilePhone(phone, 'id-ID')) {
-                        isIdentityValid = true;
-                        const text = `Oh, your name is ${name}. Your email is ${email}. Your phone number is ${phone}. Thanks!`; 
-                        console.log(`\n ${text}`);
-                        fs.writeFileSync('output.txt', text);
-                        rl.close()
+                        const newContact = { name, email, phone_number: phone };
+                        pushDataContacts(newContact);
+                        console.log('Contact saved successfully.');
+                        rl.close();
                     } else {
-                        console.log(`Your phone number is not valid! Please input valid phone number \n`);
+                        console.log('Your phone number is not valid! Please input a valid phone number.');
                         identityQuestion();
                     }
-                })
+                });
             } else {
-                console.log(`Your email is not valid! Please input valid email \n`);
+                console.log('Your email is not valid! Please input a valid email.');
                 identityQuestion();
             }
-        })
-    })
-}
+        });
+    });
+};
 
 identityQuestion();
