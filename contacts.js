0ca6ed5568f157a4442f9d5bad913038a9daccdf
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-07-26 11:38:38 +0700

    :sparkles: Add CRUD contact with yargs

--- contacts.js
+++ contacts.js
@@ -74,6 +74,81 @@ const saveContact = (newContact) => {
 };
 
 /**
+ * Fungsi untuk memperbaharui data kontak di dalam file JSON.
+ * @param {string} contactName - String nama kontak yang akan dihapus.
+ * @param {Object} updateContact - Objek kontak baru yang akan disimpan.
+ * @returns {Promise<void>} - Promise yang menyelesaikan saat kontak disimpan.
+ */
+const updateContact = (contactName, updatedContact) => {
+    return new Promise((resolve, reject) => {
+        // Mengambil data kontak yang ada dan menambah kontak baru
+        getDataContacts((contacts) => {
+            const index = contacts.findIndex(contact => contact.name === contactName);
+            if (index === -1) {
+                reject(new Error(`Contact not found.`));
+            } else {
+                contacts[index] = { ...contacts[index], ...updatedContact };
+                fs.writeFileSync('./data/contacts.json', JSON.stringify(contacts, null, 2));
+                resolve();
+            }
+        });
+    });
+};
+
+/**
+ * Fungsi untuk mendapatkan list data kontak di dalam file JSON.
+ * @returns {Promise<void>} - Promise yang menyelesaikan saat kontak disimpan.
+ */
+
+const getContactList = (callback) => {
+    getDataContacts((contacts) => {
+        let contactList = [];
+        contacts.forEach(contact => {
+            contactList.push({name: contact.name, mobile: contact.mobile});
+        });
+        callback(contactList);
+    });
+};
+
+/**
+ * Fungsi untuk detail sebuah kontak di dalam file JSON.
+ * @param {string} contactName - String nama kontak yang akan dihapus.
+ * @returns {Promise<void>} - Promise yang menyelesaikan saat kontak disimpan.
+ */
+const getContactDetail = (contactName, callback) => {
+    getDataContacts((contacts) => {
+        const index = contacts.findIndex(contact => contact.name === contactName);
+        if (index === -1) {
+            callback(new Error(`Contact not found.`), null);
+        } else {
+            // Mengirim detail kontak melalui callback
+            callback(null, contacts[index]);
+        }
+    });
+};
+
+/**
+ * Fungsi untuk menghapus kontak di dalam file JSON.
+ * @param {string} contactName - String nama kontak yang akan dihapus.
+ * @returns {Promise<void>} - Promise yang menyelesaikan saat kontak disimpan.
+ */
+const deleteContact = (contactName) => {
+    return new Promise((resolve, reject) => {
+        getDataContacts((contacts) => {
+            const index = contacts.findIndex(contact => contact.name === contactName);
+            if (index === -1) {
+                reject(new Error(`Contact not found.`));
+            } else {
+                contacts.splice(index, 1)
+                fs.writeFileSync('./data/contacts.json', JSON.stringify(contacts, null, 2));
+                console.log(`Contact has been deleted!`);
+                resolve();
+            }
+        });
+    });
+};
+
+/**
  * Fungsi untuk menanyakan input dari pengguna dengan opsi validasi.
  * @param {string} stringQuestion - Pertanyaan yang akan ditanyakan ke pengguna.
  * @param {Function|null} validate - Fungsi untuk memvalidasi jawaban pengguna. Default: null.
@@ -97,4 +172,4 @@ const questions = (stringQuestion, validate = null) => {
 };
 
 // Mengekspor fungsi-fungsi yang didefinisikan di atas
-module.exports = { questions, saveContact, isValidEmail, isValidMobile };
+module.exports = { questions, saveContact, updateContact, getContactList, getContactDetail, deleteContact, isValidEmail, isValidMobile };
