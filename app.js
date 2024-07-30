c1ad1d03ad1a1daa4c5103ce7a0daae86a10144d
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-07-30 08:55:50 +0700

    :lipstick: Styling navigation bar

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const contacts = require('./contacts');
-// const path = require('path');
+const path = require('path');
 
 // Using EJS
 app.set('view engine', 'ejs')
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Route
 app.get('/', (req, res) => {
